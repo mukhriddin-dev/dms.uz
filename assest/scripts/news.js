a22c73cf9db0780e2ff59d1312434c82230721ca
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-01-16 23:33:25 +0500

    news page added

---
+++ assest/scripts/news.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  // init: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
