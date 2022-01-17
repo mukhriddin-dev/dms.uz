c195c7e8cc3dade9879e998e82774781ec31b476
Mukhriddin <khodievm@gmail.com>
2022-01-17 13:54:02 +0500

    zzzz

---
+++ assest/scripts/Tippy.js
@@ -0,0 +1,62 @@
+tippy('#nukus', {
+  content: 'Nukus',
+});
+
+tippy('#navoi', {
+  content: 'Navoi',
+});
+
+tippy('#navoi2', {
+  content: 'Navoi',
+});
+
+
+
+
+tippy('#samarkand', {
+  content: 'Samarqand',
+});
+
+
+tippy('#sirdaryo', {
+  content: 'Sirdaryo',
+});
+
+
+tippy('#buhoro', {
+  content: "Buhoro",
+});
+
+
+tippy('#jizzakx', {
+  content: "Jizzax",
+});
+
+
+
+
+tippy('#toshkent', {
+  content: "Toshkent",
+});
+
+
+tippy('#fergan', {
+  content: "Farg'ona",
+});
+
+
+
+
+
+tippy('#fergan', {
+  content: "Farg'ona",
+});
+
+
+
+
+tippy('#fergan', {
+  content: "Farg'ona",
+});
+
+
