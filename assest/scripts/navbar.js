3d12498cf0b5440dfb9283646668a09c6676af1e
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-01-19 11:27:50 +0500

    add aos

--- assest/scripts/navbar.js
+++ assest/scripts/navbar.js
@@ -1,81 +1,68 @@
 const burgerMenu = document.querySelector(".mn");
 const navbarMenu = document.querySelector(".ul-nav");
-const loader = document.querySelector('.loader');
+const loader = document.querySelector(".loader");
 
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => (loader.style.display = "none"), 500);
 
-window.addEventListener("DOMContentLoaded",()=>{
+  // Show and Hide Navbar Menu
+  burgerMenu.addEventListener("click", () => {
+    if (navbarMenu.style.maxHeight) {
+      navbarMenu.style.maxHeight = null;
+    } else {
+      navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+    }
+    console.log(navbarMenu.scrollHeight);
+  });
 
-setTimeout(()=> (loader.style.display="none"), 500)
+  const navContent = document.querySelector(".nav_content"),
+    line = document.querySelector(".line"),
+    cabBtn = document.querySelector(".cabinet-btn"),
+    langBtn = document.querySelector(".lang-btn"),
+    tel = document.querySelector(".tel");
 
-
-
-// Show and Hide Navbar Menu
-burgerMenu.addEventListener("click", () => {
-
-
-  if (navbarMenu.style.maxHeight) {
-    navbarMenu.style.maxHeight = null;
-} else {
-    navbarMenu.style.maxHeight = navbarMenu.scrollHeight +'px';
-}
-console.log(navbarMenu.scrollHeight);
-
+  // show hide nav tabs
+  line.addEventListener("click", () => {
+    cabBtn.classList.toggle("db_none");
+    navbarMenu.classList.toggle("db_none");
+    navContent.classList.toggle("db_none");
+    langBtn.classList.toggle("opc");
+    tel.classList.toggle("opc");
+  });
 });
 
-const navContent= document.querySelector('.nav_content'),
-      line=document.querySelector('.line'),
-      cabBtn=document.querySelector('.cabinet-btn'),
-      langBtn=document.querySelector('.lang-btn'),
-      tel=document.querySelector('.tel');
+window.addEventListener("scroll", () => {
+  const userImage = document.querySelector(".user-img");
 
+  const cardImg = document.querySelectorAll(".circle-card__imgs");
+  const trucCar = document.querySelector(".truck-imgs");
+  const biker = document.querySelector(".bikers");
 
+  const st = Math.round(window.scrollY);
 
+  if (st > 15) {
+    userImage.style.transform = "translateX(-35px)";
+  } else {
+    userImage.style.transform = "translateX(0px)";
+  }
 
-// show hide nav tabs
-line.addEventListener("click", () => {
-  cabBtn.classList.toggle('db_none');
- navbarMenu.classList.toggle('db_none');
-navContent.classList.toggle('db_none');
-langBtn.classList.toggle('opc');
-tel.classList.toggle('opc');
+  if (st > 645) {
+    cardImg.forEach(
+      (item) =>
+        (item.style.transform = "translate(-50%, -50%) rotate(0deg) scale(1.2)")
+    );
+  } else {
+    cardImg.forEach(
+      (item) =>
+        (item.style.transform = "translate(-50%, -50%) rotate(-45deg) scale(1)")
+    );
+  }
 
+  if (st > 1150) {
+    trucCar.style.transform = "translateX(0px) scale(1)";
+  }
 
+  if (st > 2800) {
+    biker.style.transform = "translateX(0px) scale(1)";
+  }
 });
-
-});
-
-
-
-window.addEventListener('scroll', () => {
-
-const userImage= document.querySelector('.user-img');
-
- const cardImg=document.querySelectorAll('.circle-card__img');
- const trucCar=document.querySelector('.truck-img');
- const biker=document.querySelector('.biker');
-
-
-const st=Math.round(window.scrollY);
- 
-if(st>15){
-userImage.style.transform="translateX(-35px)";
-}else{
-  userImage.style.transform="translateX(0px)"; 
-}
-
-if(st>645){
- cardImg.forEach((item )=> item.style.transform='translate(-50%, -50%) rotate(0deg) scale(1.2)');
-}else{
-  cardImg.forEach((item )=> item.style.transform='translate(-50%, -50%) rotate(-45deg) scale(1)');
-}
-
-if(st>1150){
-  trucCar.style.transform='translateX(0px) scale(1)';
-}
-
-if(st>2800){
-  biker.style.transform='translateX(0px) scale(1)';
-}
-
-
-})
