19f47fa9833a5efcb67ce0c3bd57bfb28dfc2996
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2022-01-21 21:55:43 +0500

    push

--- assest/scripts/news.js
+++ assest/scripts/news.js
@@ -4,7 +4,8 @@ var swiper = new Swiper(".swiper-container", {
   loop: true,
   loopedSlides: 50,
   autoplay: {
-    delay: 2500,
+    delay: 2000,
+    disableOnInteraction: false,
   },
   // init: false,
   pagination: {
